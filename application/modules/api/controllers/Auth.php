20d5e8c1d3071ff8a223ef2f55b0d593e6cc1500
chanomkaimook <passakornthemoon@gmail.com>
2024-05-09 19:55:32 +0700

    Update Auth.php

--- application/modules/api/controllers/Auth.php
+++ application/modules/api/controllers/Auth.php
@@ -18,6 +18,7 @@ class Auth extends API_Controller
 
     public function index()
     {
+        header("Access-Control-Allow-Origin: *");
         // API Configuration
         $this->_apiConfig([
             'methods' => ['GET'], // Request Execute Only POST and GET Method
